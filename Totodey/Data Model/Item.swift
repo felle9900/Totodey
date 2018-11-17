3e6326fcebbf692244aed4703e41317685a16ad0
Jacob Felsted <jacobfelsted@me.com>
2018-11-17 10:55:47 +0100

    Convert to MVC design pattern

---
+++ Totodey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Totodey
+//
+//  Created by Jacob Felsted on 17/11/2018.
+//  Copyright © 2018 Jacob Felsted. All rights reserved.
+//
+
+import Foundation
+
+class Item  {
+    var title : String = ""
+    var done: Bool = false
+}
