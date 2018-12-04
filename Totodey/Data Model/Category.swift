542fac40f656292ef22b6666dc4899d82cf87687
Jacob Felsted <jacobfelsted@me.com>
2018-12-04 15:36:33 +0100

    Using Realm database

---
+++ Totodey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Totodey
+//
+//  Created by Jacob Felsted on 04/12/2018.
+//  Copyright © 2018 Jacob Felsted. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name : String = ""
+    let items = List<Item>()
+}
